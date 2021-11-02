ede362f62fb2b86b0664fffb253cc4f2c492af04
DhruvChhura <dhruvchhura18@gmail.com>
2021-11-02 15:23:02 +0530

    ysl: Bump build fp to A12 Coral November

--- derp_ysl.mk
+++ derp_ysl.mk
@@ -30,8 +30,12 @@ TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
+# Build fingerprint
+BUILD_FINGERPRINT := google/coral/coral:12/SP1A.211105.002/7743617:user/release-keys
+BUILD_DESCRIPTION := coral-user 12 SP1A.211105.002 7743617 release-keys
 
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
